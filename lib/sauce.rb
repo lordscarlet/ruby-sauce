ae122702a28961cf66e07fb09301d4b575c08d8c
Brian Cassidy <bricas@bricas-laptop.(none)>
2008-07-25 12:50:02 -0300

    fix data field bug. add some error checking.

--- lib/sauce.rb
+++ lib/sauce.rb
@@ -7,16 +7,25 @@ class Sauce
   
   def initialize(filename)
     @data = Array.new
-    i = 1
+    @has_sauce = false
+
     begin
       file = File.new(filename, "r")
+      file.binmode
+
+      raise if file.stat.size < 128
+
       file.seek(-128, IO::SEEK_END)
       raw_sauce = file.read(128)
+
+      file.close
+
+      raise if raw_sauce !~ "^SAUCE"
+
       @data = raw_sauce.unpack("A5 A2 A35 A20 A20 A8 V C C v v v v C C A22")
       @has_sauce = true
     rescue => err
       
-      @has_sauce = false
     end  
   end
   
@@ -40,51 +49,47 @@ class Sauce
     @data[4]
   end
   
-  def group
+  def date
     @data[5]
   end
   
-  def date
+  def filesize
     @data[6]
   end
   
-  def filesize
+  def datatype_id
     @data[7]
   end
   
-  def datatype_id
+  def filetype_id
     @data[8]
   end
   
-  def filetype_id
+  def tinfo1
     @data[9]
   end
   
-  def tinfo1
+  def tinfo2
     @data[10]
   end
   
-  def tinfo2
+  def tinfo3
     @data[11]
   end
   
-  def tinfo3
+  def tinfo4
     @data[12]
   end
   
-  def tinfo4
+  def comments
     @data[13]
   end
   
-  def comments
+  def flags_id 
     @data[14]
   end
   
-  def flags_id 
-    @data[15]
-  end
-  
   def filler
-    @data[16]
+    @data[15]
   end
 end
